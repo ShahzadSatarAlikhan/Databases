7f0b8385d2cc184e868bd801ffe20d6e4fba847b
ShahzadSatarAlikhan <77251563+ShahzadSatarAlikhan@users.noreply.github.com>
2021-02-21 19:14:08 -0500

    Add files via upload

---
+++ openweathermap-master/app.js
@@ -0,0 +1,25 @@
+var button = document.querySelector('.button')
+
+var inputValue = document.querySelector('.inputValue')
+var name = document.querySelector('.name');
+var desc = document.querySelector('.desc');
+var temp = document.querySelector('.temp');
+
+
+
+button.addEventListener('click', function(){
+    fetch('https://api.openweathermap.org/data/2.5/weather?q=' + inputValue.value +'&appid=135ee385d980c62a2d96708d423e1b77&units=metric')
+.then(response => response.json())
+    .then(data => {
+        var nameValue = data['name'];
+        var tempValue = data['main']['temp'];
+        var descValue = data['weather'][0]['description'];
+
+        name.innerHTML = nameValue;
+        temp.innerHTML = tempValue;
+        desc.innerHTML = descValue;
+    })
+
+.catch (err => alert("Wrong city name entered!"));
+
+    })
